06a21584e618222b36814ab7351b3c4e814e0099
5540011055 <izudsuarez@gmail.com>
2018-08-16 18:32:40 +0700

    123

--- fifa55sport.com/pages/index.php
+++ fifa55sport.com/pages/index.php
@@ -3,7 +3,8 @@
     background-repeat: no-repeat;
     background-size: 100%;    background-size: 1920px 490px;">
     <img src="images/promo_index.png" width="100%" class="hidden-md">
-    <img src="images/promotion_mb/promo_phone.png" width="100%" class="hidden-lg" >
+    <div class="hidden-lg"></div>
+    <img src="images/promotion_mb/v2/promo.png" />
   <div class="wrapper section-promotion clearfix" style="    padding-bottom: 0px; margin-top: 0px; display: none;">
     <div class="row">
       <div class="">
